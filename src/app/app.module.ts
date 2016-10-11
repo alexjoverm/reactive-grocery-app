b50fdf9130eff5a3160bd7866d93bfd41a455b44
Alex J <alexjovermorales@gmail.com>
2016-10-11 19:44:31 +0200

    Use toolbar on the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule }      from '@angular/core'
+import { FormsModule }   from '@angular/forms'
+import { HttpModule }    from '@angular/http'
 
-import { AppComponent } from './app.component';
+import { MaterialModule } from '@angular/material'
+
+import { AppComponent }       from './app.component'
+import { ListComponent }      from './components/list/list.component'
+import { GroceriesComponent } from './containers/groceries/groceries.component';
+import { ListEditComponent } from './components/list-edit/list-edit.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    GroceriesComponent,
+    ListEditComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    // Add material module
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/containers/groceries/groceries.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-groceries',
+  templateUrl: './groceries.component.html',
+  styleUrls: ['./groceries.component.scss']
+})
+export class GroceriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
